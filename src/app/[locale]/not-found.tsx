3759738608f51f15d8415538b5fa8d8bedf688bc
roaziy <erkhemtur389@gmail.com>
2025-06-01 20:35:18 +0800

    not found page done

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image';
 
 // Import Navbar Item Route
 import { useNavItems } from "@/components/navbar/navItems";
@@ -7,10 +8,27 @@ export default function NotFound() {
   const navItems = useNavItems();
   const homeItem = navItems[0];
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href={homeItem.anchor}>Return Home</Link>
+    <div className='min-h-screen w-full flex items-center justify-center px-4'>
+      <div className='py-12 flex flex-col md:flex-row gap-8 md:gap-16 lg:gap-24 items-center justify-center'>
+        <Image
+          src="/images/notfound/notfound.jpg"
+          alt="404 Not Found"
+          width={150}
+          height={150}
+          className="select-none w-24 md:w-32 lg:w-40 h-auto"
+          draggable="false"
+        />
+        <div className='flex flex-col items-center md:items-start justify-center text-center md:text-left'>
+          <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold mb-2'>404 - Not Found!</h1>
+          <p className='mb-6 text-gray-600'>Could not find requested resource</p>
+          <Link 
+            href={homeItem.anchor} 
+            className='px-6 py-3 bg-white hover:bg-gray-100 transition-colors rounded-full border border-black text-center'
+          >
+            Back to Home
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
